2cdfed25bbd9ba93b94ca3656184acdbc64d4d22
Quang Do <qdo1@nd.edu>
2018-07-28 14:35:48 -0400

    added getters and setters

--- src/LifeGame.java
+++ src/LifeGame.java
@@ -1,4 +1,11 @@
 
 public class LifeGame {
-
+	public LifeGame() {
+		
+	}
+	
+	public static void main(String [] args) {
+		Person temp = new Person(100);
+		
+	}
 }
--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,91 @@
 
 public class Person {
+	private int id;
+	private int age;
+	private int health;
+	private char gender;
+	private String firstName;
+	private String lastName;
+	private Person mother;
+	private Person father;
+	private boolean Alive;
+	private Person[] children;
+	
+	
+	public Person(int id) {
+		this.id = id;
+	}
+	public Person(Person mother, Person father) {
+		setMother(mother);
+		setFather(father);
 
+	}
+	
+	
+	/* -------- GETTERS & SETTERS -------- */
+	public int getAge() {
+		return age;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Person getMother() {
+		return mother;
+	}
+	public void setMother(Person mother) {
+		this.mother = mother;
+	}
+	public Person getFather() {
+		return father;
+	}
+	public void setFather(Person father) {
+		this.father = father;
+	}
+	public boolean isAlive() {
+		return Alive;
+	}
+	public void setAlive(boolean alive) {
+		Alive = alive;
+	}
+	public Person[] getChildren() {
+		return children;
+	}
+	public void setChildren(Person[] children) {
+		this.children = children;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	/* -------- METHODS -------- */
+	
+	
+	
 }
